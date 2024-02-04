fec62f4e19d1742d78dd9cd313d1623e9d09b6ed
bookiosk <bookiosk@163.com>
2024-02-05 07:08:24 +0800

    fix: 完成邮箱输入表单欠缺膜太狂校验

--- src/views/home/components/forget-form.vue
+++ src/views/home/components/forget-form.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="register-form-wrapper">
-    <a-button type="primary" long @click="handleChange('loginForm')"
-      >忘记</a-button
+    <a-form
+      ref="formRef"
+      :model="form"
+      style="width: 300px"
+      :rules="rules"
+      @submit="handleSubmit"
     >
+      <a-form-item field="email" label="email">
+        <a-input
+          v-model="form.email"
+          placeholder="please enter your email..."
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" long html-type="submit">获取验证码</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { defineEmits } from 'vue';
+  import { defineEmits, ref } from 'vue';
+  import { ValidatedError } from '@arco-design/web-vue/es/form/interface';
 
   const emit = defineEmits(['changeForm']);
-  const handleChange = (value: string) => {
-    emit('changeForm', value);
+  const formRef = ref(null);
+  const form = ref({
+    email: '',
+  });
+  // 表单校验规则
+  const rules = {
+    email: [
+      {
+        type: 'email',
+        required: true,
+      },
+    ],
+  };
+  const handleSubmit = async ({
+    errors,
+    values,
+  }: {
+    errors: Record<string, ValidatedError> | undefined;
+    values: Record<string, any>;
+  }) => {
+    if (errors) return;
+    emit('changeForm', 'loginForm');
   };
 </script>
 
--- src/views/home/locale/en-US.ts
+++ src/views/home/locale/en-US.ts
@@ -19,4 +19,5 @@ export default {
     'Internationalization, routing configuration, state management everything',
   'login.banner.slogan3': 'Access visualization enhancement tool AUX',
   'login.banner.subSlogan3': 'Realize flexible block development',
+  'forget.email.placeholder': 'please enter your email...',
 };
